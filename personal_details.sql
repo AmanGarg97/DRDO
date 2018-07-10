83fa95585be8090acdbf4923bb4064508728f753
AmanGarg <amangarg34.ag@gmail.com>
2018-07-10 22:39:17 +0530

    database updated

--- personal_details.sql
+++ personal_details.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 23, 2018 at 10:10 AM
+-- Generation Time: Jul 10, 2018 at 04:26 PM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -40,14 +40,14 @@ CREATE TABLE IF NOT EXISTS `academic_details` (
   `company3` text,
   `enroll_id` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `academic_details`
 --
 
 INSERT INTO `academic_details` (`id`, `adm_year`, `pass_year`, `Branch`, `spec`, `company1`, `company2`, `company3`, `enroll_id`) VALUES
-(1, 2015, 2017, 'CSE', 'aman', 'aman', 'aman2', 'aman3', '');
+(7, 2016, 2020, 'CSE', 'Web Development', 'DRDO', '', '', '1234');
 
 -- --------------------------------------------------------
 
@@ -70,16 +70,19 @@ CREATE TABLE IF NOT EXISTS `students` (
   `pin_code` int(10) DEFAULT NULL,
   `phone` bigint(15) DEFAULT NULL,
   `email_id` varchar(25) DEFAULT NULL,
+  `telephone` bigint(12) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` varchar(15) NOT NULL DEFAULT 'Pending',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `first_name`, `last_name`, `enroll_id`, `b_day`, `gender`, `father_name`, `mother_name`, `address`, `state`, `pin_code`, `phone`, `email_id`) VALUES
-(12, 'aman', 'aman', '123', '2015-12-02', NULL, 'aman', 'aman', 'aman', 'aman', 110032, 952632568, 'amangarg34.ag@gmail.com'),
-(11, 'aman', 'aman', '123', '2015-12-02', NULL, 'aman', 'aman', 'aman', 'aman', 110032, 952632568, 'amangarg34.ag@gmail.com');
+INSERT INTO `students` (`id`, `first_name`, `last_name`, `enroll_id`, `b_day`, `gender`, `father_name`, `mother_name`, `address`, `state`, `pin_code`, `phone`, `email_id`, `telephone`, `password`, `status`) VALUES
+(24, 'Aman', 'Garg', '1234', '0001-01-01', NULL, 'Aman', 'Aman', 'Shahdara Delhi ', 'Delhi ', 110032, 985632569, 'amangarg34.ag@gmail.com', 9563256, 'aman', 'Accepted'),
+(25, 'Naman', 'Garg', '1234', '2000-02-12', NULL, 'aman', 'aman', 'shahdara delhi ', 'Delhi ', 110036, 798523654, 'a@gmail.com', 789632562, 'naman', 'Pending');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
